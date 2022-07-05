2ff2d5c92bcef4cfd448f4bc95c93458419283d0
Kairuus <kingkairuus@gmail.com>
2022-07-05 13:50:18 -0300

    Inserção de usuário com post

--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.workshopmongo.domain.Person;
+import com.nelioalves.workshopmongo.dto.UserDTO;
 import com.nelioalves.workshopmongo.repository.UserRepository;
 import com.nelioalves.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 	public Person findById(String id) {
 		Optional<Person> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public Person insert(Person obj) {
+		return repo.insert(obj);
+	}
+	
+	public Person fromDTO(UserDTO objDto) {
+		return new Person(objDto.getId(),objDto.getName(),objDto.getEmail());
 	}
 }
